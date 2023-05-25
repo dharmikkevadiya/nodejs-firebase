66917e65ed4342332fba46d2eaaf14095cb8f0b1
Dharmik Kevadiya <dharmikkevadiya1552@gmail.com>
2023-05-25 16:06:32 +0530

    Remove default config

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const MONGODB_URL =
   process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/demo1";
 const APP_URL = process.env.APP_URL || "http://localhost:5000";
-const JWT_SECRET = process.env.JWT_SECRET || "hellojwt123";
+const JWT_SECRET = process.env.JWT_SECRET;
 const BUCKET_URL = process.env.BUCKET_URL;
 const PORT = process.env.PORT || 5000;
 
